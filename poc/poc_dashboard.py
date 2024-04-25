25bb3b4b0e5a6010b0cba81b17d834eb28ba472c
Jacques Sham <jacquessham2015@gmail.com>
2024-04-24 20:44:26 -0700

    Improve the data and chart

--- poc/poc_dashboard.py
+++ poc/poc_dashboard.py
@@ -1,20 +1,29 @@
+import json
 import pandas as pd
 import plotly
 import plotly.graph_objs as go
 from plotly.offline import *
+from boa_docs import *
 
 
 # To initiate ploty to run offline
 init_notebook_mode(connected=True)
 # Import data
-df_chkacct = pd.read_csv('boa_chk_example.csv')
+with open('poc_arguements.json') as f:
+	arg = json.load(f)
+
+df_chkacct = read_boacsv(arg['boa_chk'][0])
+
 
 # Data
 data = []
 data.append(go.Scatter(x=df_chkacct['Date'], y=df_chkacct['Running_Bal'],
 					mode='lines'))
 # Layout
-layout = {'title':{'text':'Number of Customer Trend', 'x':0.5}}
+layout = {
+	'title':{'text':'Personal Net Worth', 'x':0.5},
+	'plot_bgcolor':'rgba(0,0,0,0)'
+}
 
 fig = go.Figure(data=data, layout=layout)
 
