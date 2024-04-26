1bd110e1ae11766a901f5f9677480b243bc9c27e
Jacques Sham <jacquessham2015@gmail.com>
2024-04-25 20:50:16 -0700

    Add donut pie viz

--- poc/poc_dashboard.py
+++ poc/poc_dashboard.py
@@ -13,8 +13,9 @@ with open('poc_arguements.json') as f:
 	arg = json.load(f)
 
 df_chkacct = read_boacsv(arg['boa_chk'][0])
+df_cc = read_boacsv(arg['boa_cc'][0])
 
-
+## Checking Account
 # Data
 data = []
 data.append(go.Scatter(x=df_chkacct['Date'], y=df_chkacct['Running_Bal'],
@@ -28,3 +29,19 @@ layout = {
 fig = go.Figure(data=data, layout=layout)
 
 plotly.offline.plot(fig, filename='chk_balance_viz.html')
+
+## Credit Expense
+# Data
+data = []
+data.append(go.Pie(labels=df_cc['Expense_Category'], 
+					values=df_cc[df_cc['Amount']>0]['Amount'],
+					hole=0.6))
+# Layout
+layout = {
+	'title':{'text':'Monthly Expense', 'x':0.5},
+	'plot_bgcolor':'rgba(0,0,0,0)'
+}
+
+fig = go.Figure(data=data, layout=layout)
+
+plotly.offline.plot(fig, filename='chk_cc_viz.html')
